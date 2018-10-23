aa9acc31e363b930bd9f4a1c0d63bd7a9a30694f
Theo Leonard P. Tejada <35455761+kkshinichi@users.noreply.github.com>
2018-10-23 15:39:05 +0800

    Source code for the control of the motor

---
+++ servo sample.js
@@ -0,0 +1,25 @@
+var raspi = require('raspi-io');
+var five = require('johnny-five');
+var board = new five.Board({
+	io: new raspi()
+});
+
+board.on('ready', function() {
+	var toggle = new five.Switch(5);
+	var servo = new five.Servo(1);
+	this.repl.inject({
+    servo: servo,
+    toggle: toggle
+  });
+
+	toggle.on("close", function() {
+		console.log("Going Up");
+		servo.center();
+	});
+	toggle.on("open", function() {
+		console.log("Going Down");
+		servo.min();
+	});
+});
+
+// 180-270
